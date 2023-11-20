435a3da8b948bf94c59ce21445d532fc25eb420d
denjo1234 <151548794+denjo1234@users.noreply.github.com>
2023-11-20 23:17:36 +0800

    Add files via upload

---
+++ soduk.py
@@ -0,0 +1,17 @@
+import time
+
+
+def update(self):
+        if self.start_game:
+            if self.tiles_grid == self.tiles_grid_completed:
+                self.start_game = False
+                if self.high_score > 0:
+                    self.high_score = self.elapsed_time if self.elapsed_time < self.high_score else self.high_score
+                else:
+                    self.high_score = self.elapsed_time
+                self.save_score()
+
+            if self.start_timer:
+                self.timer = time.time()
+                self.start_timer = False
+            self.elapsed_time = time.time() - self.timer
